412944f652784662c358d536e3930a7aeae72d79
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-06-14 22:23:26 +0530

    added aos effects on about section

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import about from './About.module.css'
+import Aos from 'aos'
 const About = () => {
+    useEffect(()=>{
+        Aos.init({ duration: 1500 });
+    },[])
   return (
     <div>
-        <p className={about.title}>About</p>
-        <div className={about.desc}>
+        <p data-aos="fade-right" className={about.title}>About</p>
+        <div data-aos="fade-left" className={about.desc}>
         Lorem ipsum dolor sit amet, consectetur 
 adipiscing elit. Arcu suspendisse nisi commodo, posuere quis cras natoque. At est ultricies vestibulum porttitor at magna mi volutpat dis. Pretium ornare cras rhoncus, feugiat nisi nec. Semper eget elementum venenatis faucibus. Id sit morbi senectus sed turpis ut. Turpis sollicitudin pellentesque sem feugiat sed.
 <br />
 Lorem ipsum dolor sit amet, consectetur 
 adipiscing elit. Arcu suspendisse nisi commodo, posuere quis cras natoque. At est ultricies vestibulum porttitor at magna mi volutpat dis. Pretium ornare cras rhoncus, feugiat nisi nec. Semper eget elementum venenatis faucibus. Id sit morbi senectus sed turpis ut. Turpis sollicitudin pellentesque sem feugiat sed.feugiat nisi nec. Semper eget elementum venenatis faucibus. Id sit morbi senectus sed turpis ut. Turpis sollicitudin pellentesque sem feugiat sed.
         </div>
-        <div className={about.greenBar}>
-            
+        <div  data-aos="fade-up-right" className={about.greenBar}>
+
         </div>
     </div>
   )
