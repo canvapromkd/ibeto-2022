0f80baebe3d313e2e8c1db9f8daf8abd6d9a7e0b
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-06-29 17:55:16 +0530

    footer section created

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -4,6 +4,7 @@ import About from '../../sections/About/About'
 import Eventformat from '../../sections/Eventformat/Eventformat'
 import Faq from '../../sections/Faq/Faq'
 import Contact from '../../sections/Contact/Contact'
+import Footer from '../../sections/Footer/Footer'
 function MainPage() {
   return (
     <div>
@@ -12,6 +13,7 @@ function MainPage() {
         <Eventformat/>
         <Faq/>
         <Contact/>
+        <Footer/>
     </div>
   )
 }
