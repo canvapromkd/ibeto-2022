7a518ee80b072f0dd5d8443c47597174efeb94c9
Hemanth R <hemanthrt18@gmail.com>
2022-09-13 12:32:17 +0530

    p sts

--- src/sections/ProblemStmt/ProblemStmt.js
+++ src/sections/ProblemStmt/ProblemStmt.js
@@ -1,12 +1,50 @@
 import React from 'react'
 
+import ProblemStmtCard from './ProblemStmtCard'
+
 import './ProblemStmt.css'
+
+
+const problemStatements = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+  {
+    id: 5,
+  },
+  {
+    id: 6,
+  },
+  {
+    id: 7,
+  },
+  {
+    id: 8,
+  },
+]
 
 const ProblemStmt = () => {
   return (
     <div className='problemStmt'>
       <div className='problemStmt__container'>
         <h1 className='section__header'>Problem Statements</h1>
+
+        <div className='problemStmt__cards'>
+          {problemStatements.map((ps) => (
+            <ProblemStmtCard 
+              key={ps.id} 
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
---
+++ src/sections/ProblemStmt/ProblemStmtCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProblemStmtCard() {
+  return (
+    <div className='problemStmtCard'>
+        <h2>Safe Transportation</h2>
+        <img src='' alt='' />
+        <button>View Details</button>
+    </div>
+  )
+}
+
+export default ProblemStmtCard
