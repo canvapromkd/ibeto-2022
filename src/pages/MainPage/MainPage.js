31c8dbbbde4a29953167b6a144f181d309fdc5f1
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-09-17 10:30:22 +0530

    done navbar

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 import { Landing, About, Eventformat, Faq, TimelineComponent, Contact, Footer, Rewards, ProblemStmt, WhyParticipate, WhatsNew } from '../../sections'
 
 function MainPage() {
   return (
     <div>
+      <Navbar/>
       <Landing/>
       <About/>
       <Eventformat/>
