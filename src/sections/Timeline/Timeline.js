e874fba67f10887bc0113d0aa48e0ef6e1ac9caf
Adithya Ajith <4adhi007@gmail.com>
2022-09-19 23:15:13 +0530

    contentadd timeline

--- src/sections/Timeline/Timeline.js
+++ src/sections/Timeline/Timeline.js
@@ -31,7 +31,7 @@ useEffect(()=>{
             contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
             date="2011 - present"
           >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
+            <h3 className="vertical-timeline-element-title">IBeTo Launch - Registration Opens</h3>
            
             <div  className='timeline_content'>
               
@@ -53,6 +53,93 @@ useEffect(()=>{
               Creative Direction, User Experience, Visual Design, SEO, Online Marketing
             </p>
           </VerticalTimelineElement>
+          <VerticalTimelineElement 
+            
+            className="vertical-timeline-element--work "
+            
+            contentStyle={{color:'#ffff', background: 'linear-gradient(to right,#1BABC3,#61C5D4 ,#22B7CB, #2394B4)'}}
+            contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
+            date="2011 - present"
+          >
+            <h3 className="vertical-timeline-element-title">IBeTo Launch - Registration Opens</h3>
+           
+            <div  className='timeline_content'>
+              
+            </div>
+            <p className='timeline__para'>
+              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+         contentStyle={{background:'linear-gradient(to right,#66A5A0,#8FCCAD,#B1D89F,#9ACE7C'}}
+         contentArrowStyle={{borderRight:'7px solid #11774b'}}
+          date="2010 - 2011"
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          
+          <p className='timeline__para' >
+            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement 
+            
+            className="vertical-timeline-element--work "
+            
+            contentStyle={{color:'#ffff', background: 'linear-gradient(to right,#1BABC3,#61C5D4 ,#22B7CB, #2394B4)'}}
+            contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
+            date="2011 - present"
+          >
+            <h3 className="vertical-timeline-element-title">IBeTo Launch - Registration Opens</h3>
+           
+            <div  className='timeline_content'>
+              
+            </div>
+            <p className='timeline__para'>
+              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          
+
+          <VerticalTimelineElement 
+            
+            className="vertical-timeline-element--work "
+            
+            contentStyle={{color:'#ffff', background: 'linear-gradient(to right,#1BABC3,#61C5D4 ,#22B7CB, #2394B4)'}}
+            contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
+            date="2011 - present"
+          >
+            <h3 className="vertical-timeline-element-title">IBeTo Launch - Registration Opens</h3>
+           
+            <div  className='timeline_content'>
+              
+            </div>
+            <p className='timeline__para'>
+              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          
+
+          <VerticalTimelineElement 
+            
+            className="vertical-timeline-element--work "
+            
+            contentStyle={{color:'#ffff', background: 'linear-gradient(to right,#1BABC3,#61C5D4 ,#22B7CB, #2394B4)'}}
+            contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
+            date="2011 - present"
+          >
+            <h3 className="vertical-timeline-element-title">IBeTo Launch - Registration Opens</h3>
+           
+            <div  className='timeline_content'>
+              
+            </div>
+            <p className='timeline__para'>
+              Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          
+          
         </VerticalTimeline>
         </div>
       </div>
