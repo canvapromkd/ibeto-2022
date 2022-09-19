90230f4bd5fe0e5796b4fae9cdda9278f9dda81a
jithin-j <jithinjagadeesh1@gmail.com>
2022-09-19 19:44:41 +0530

    Changes in why participate

--- src/sections/WhyParticipate/WhyParticipate.js
+++ src/sections/WhyParticipate/WhyParticipate.js
@@ -1,15 +1,15 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from "react";
 import Slider from "react-slick";
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
-import useMediaQuery from '@mui/material/useMediaQuery';
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-import WhyParticipateCard from './WhyParticipateCard';
-import ellipse1 from "../../assets/svg/Ellipse8.svg"
-import ellipse2 from "../../assets/svg/Ellipse10.svg"
-import './WhyParticipate.css'
+import WhyParticipateCard from "./WhyParticipateCard";
+import ellipse1 from "../../assets/svg/Ellipse8.svg";
+import ellipse2 from "../../assets/svg/Ellipse10.svg";
+import "./WhyParticipate.css";
+import Aos from "aos";
 
 const WhyParticipate = () => {
-
   const sliderRef = useRef();
 
   const settings = {
@@ -27,30 +27,42 @@ const WhyParticipate = () => {
 
   const gotoNext = () => {
     sliderRef.current.slickNext();
-  }
+  };
 
   const gotoPrev = () => {
     sliderRef.current.slickPrev();
-  }
-  
-  const mobileView = useMediaQuery('(max-width:1400px)');
- 
+  };
 
+  const mobileView = useMediaQuery("(max-width:1400px)");
+  useEffect(() => {
+    Aos.init({ duration: 1100 });
+  }, []);
   return (
-    <div className='whyParticipate'>
-      <div className='whyParticipate__container'>
-        <div className='wpC__left'>
-          <h2 className='wpc__header'>Why Participate?</h2>
-          {mobileView &&
-          <div className='wpc_mobile_animation'>
-            <img src="https://avatars.githubusercontent.com/u/43471295?v=4" alt="" />
+    <div className="whyParticipate">
+      <div className="whyParticipate__container" data-aos="zoom-in">
+        {mobileView || (
+          <div className="wpC__left">
+            <img
+              src="https://avatars.githubusercontent.com/u/43471295?v=4"
+              alt="-"
+            />
           </div>
-          }
-          <img className='ellipse el_1' src={ellipse1} alt="vector"/>
-          <img className='ellipse el_2' src={ellipse2} alt="vector"/>
-          <img className='ellipse el_3' src={ellipse2} alt="vector"/>
-          <div className='wpC_slider'>
-            <IoIosArrowUp className='slider__arrow' onClick={gotoNext}/>
+        )}
+        <div className="wpC__right">
+          <h2 className="wpc__header">Why Participate?</h2>
+          {mobileView && (
+            <div className="wpc_mobile_animation">
+              <img
+                src="https://avatars.githubusercontent.com/u/43471295?v=4"
+                alt=""
+              />
+            </div>
+          )}
+          <img className="ellipse el_1" src={ellipse1} alt="vector" />
+          <img className="ellipse el_2" src={ellipse2} alt="vector" />
+          <img className="ellipse el_3" src={ellipse2} alt="vector" />
+          <div className="wpC_slider">
+            <IoIosArrowUp className="slider__arrow" onClick={gotoNext} />
             <Slider {...settings} ref={sliderRef} className="wpcSlider">
               <div>
                 <WhyParticipateCard />
@@ -77,15 +89,12 @@ const WhyParticipate = () => {
                 <WhyParticipateCard />
               </div>
             </Slider>
-            <IoIosArrowDown className='slider__arrow' onClick={gotoPrev}/>
+            <IoIosArrowDown className="slider__arrow" onClick={gotoPrev} />
           </div>
         </div>
-        {mobileView ||  <div className='wpC__right'>
-          <img src="https://avatars.githubusercontent.com/u/43471295?v=4" alt="-" />
-        </div>}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhyParticipate
+export default WhyParticipate;
