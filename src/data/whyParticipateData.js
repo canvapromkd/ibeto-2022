5ac3be41b27814488faa152b9ec87282e90cbc9a
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-09-20 22:58:32 +0530

    added logos for y p

--- src/data/whyParticipateData.js
+++ src/data/whyParticipateData.js
@@ -1,8 +1,11 @@
-import {GiProgression} from 'react-icons/gi'
+import {GiProgression,GiTeacher,GiTargetPrize,GiCash,GiPublicSpeaker,GiMeshNetwork,GiBrainstorm} from 'react-icons/gi'
+import {MdMapsHomeWork} from 'react-icons/md'
+import {RiKakaoTalkFill} from 'react-icons/ri'
 export const whyParticipateData=[
     {
         id:1,
         content:'Bring a brilliant idea and help it transform and make the difference it deserves.',
+        icon:<GiBrainstorm/>
     },
     {
         id:2,
@@ -12,36 +15,36 @@ export const whyParticipateData=[
     {
         id:3,
         content:'Get the chance to avail internships from renowned corporate companies.',
-        icon: <GiProgression/>
+        icon: <MdMapsHomeWork/>
     },
     {
         id:4,
         content:'Be privy to valuable advice from a panel of accomplished experts willing to help you along any point you may be stuck in your journey.',
-        icon: <GiProgression/>
+        icon: <GiTeacher/>
     },
     {
         id:5,
         content:'Take a shot at winning prizes worth up to 1 Lakh, which are open to the top 3 winning teams.',
-        icon: <GiProgression/>
+        icon: <GiTargetPrize/>
     },
     {
         id:6,
         content:'Get access to premium talks and workshops to sharpen your knowledge.',
-        icon: <GiProgression/>
+        icon: <RiKakaoTalkFill/>
     },
     {
         id:7,
         content:'Get 50 activity points for socially relevant projects, provided as per KTU regulations.',
-        icon: <GiProgression/>
+        icon: <GiCash/>
     },
     {
         id:8,
         content:'Attend technical sessions hosted by speakers, who are more than familiar with each aspect of project development.',
-        icon: <GiProgression/>
+        icon: <GiPublicSpeaker/>
     },
     {
         id:9,
         content:'Build strong networks and connect with professionals, individuals working on socially relevant projects, and more.',
-        icon: <GiProgression/>
+        icon: <GiMeshNetwork/>
     },
 ]
