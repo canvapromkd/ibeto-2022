7eb9bd76d52ee3d8b7bbbd416201f746d79d11fd
jithin-j <jithinjagadeesh1@gmail.com>
2022-09-20 22:29:07 +0530

    Final Tasks

--- src/sections/Landing/Landing.js
+++ src/sections/Landing/Landing.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import './Landing.css'
-import ibeto_22 from '../../assets/video/Gear Animation_sized_down_BG.mp4'
-import Aos from 'aos'
-import { useEffect } from 'react'
+import React from "react";
+import "./Landing.css";
+import ibeto_22 from "../../assets/video/New.mp4";
+import Aos from "aos";
+import { useEffect } from "react";
 const Landing = () => {
-  useEffect(()=>{
-    Aos.init({duration:1100})
-  },[])
+  useEffect(() => {
+    Aos.init({ duration: 1100 });
+  }, []);
   return (
-    <div className='landing_main'id="landing">
+    <div className="landing_main" id="landing">
       <div data-aos="zoom-in">
-        <video className='video_container' autoPlay loop muted playsInline>
-          <source src={ibeto_22} type='video/Mp4'/>
+        <video className="video_container" autoPlay loop muted playsInline>
+          <source src={ibeto_22} type="video/Mp4" />
         </video>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
--- src/sections/WhatsNew/WhatsNew.js
+++ src/sections/WhatsNew/WhatsNew.js
@@ -1,56 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { RiLightbulbFlashLine } from "react-icons/ri";
-import WhatsNewCard from './WhatsNewCard'
-import './WhatsNew.css'
-import Aos from 'aos';
+import WhatsNewCard from "./WhatsNewCard";
+import "./WhatsNew.css";
+import Aos from "aos";
 
 const whatsNewData = [
-    {
-        id: 1,
-        text: 'Separate Tracks',
-        icon: <RiLightbulbFlashLine className='wn__icon'/>
-    },
-    {
-        id: 2,
-        text: 'Aid to the contestants',
-        icon: <RiLightbulbFlashLine className='wn__icon'/>
-    },
-    {
-        id: 3,
-        text: 'Incentives during project development',
-        icon: <RiLightbulbFlashLine className='wn__icon'/>
-    },
-    {
-        id: 4,
-        text: 'Community representations',
-        icon: <RiLightbulbFlashLine className='wn__icon'/>
-    },
-]
+  {
+    id: 1,
+    text: "Separate Tracks",
+    icon: <RiLightbulbFlashLine className="wn__icon" />,
+  },
+  {
+    id: 2,
+    text: "Aid To The Contestants",
+    icon: <RiLightbulbFlashLine className="wn__icon" />,
+  },
+  {
+    id: 3,
+    text: "Incentives During Project Development",
+    icon: <RiLightbulbFlashLine className="wn__icon" />,
+  },
+  {
+    id: 4,
+    text: "Community Representations",
+    icon: <RiLightbulbFlashLine className="wn__icon" />,
+  },
+];
 
 function WhatsNew() {
-    useEffect(()=>{
-        Aos.init({duration:1100})
-    })
+  useEffect(() => {
+    Aos.init({ duration: 1100 });
+  });
   return (
-    <div className='whatsNew' id="whatsNew">
-        
-        <div className='whatsNew__comntainer' >
-            <h1 className='ps__header'>What's New</h1>
+    <div className="whatsNew" id="whatsNew">
+      <div className="whatsNew__comntainer">
+        <h1 className="ps__header">What's New</h1>
 
-            <div className='whatsNew__cards' data-aos="zoom-out">
-                {whatsNewData.map((wn) => (
-                    <WhatsNewCard 
-                        key={wn.id}
-                        id={wn.id}
-                        text={wn.text}
-                        icon={wn.icon}
-                    />
-                ))}
-            </div>
+        <div className="whatsNew__cards" data-aos="zoom-out">
+          {whatsNewData.map((wn) => (
+            <WhatsNewCard
+              key={wn.id}
+              id={wn.id}
+              text={wn.text}
+              icon={wn.icon}
+            />
+          ))}
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default WhatsNew
+export default WhatsNew;
