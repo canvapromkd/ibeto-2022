cae9d6bd7f27869376fa96b81e3244b4d7c47e86
Hemanth R <hemanthrt18@gmail.com>
2022-09-20 08:37:25 +0530

    .

---
+++ src/sections/Testimonials/TestimonialCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaQuoteLeft } from "react-icons/fa";
+
+function TestimonialCard({ id, name, text, designation, image, batch }) {
+  return (
+    <div className='testimonialCard' key={id}>
+      <div className='testimonial_content'>
+        <div className='test_text'>
+          <FaQuoteLeft className='quotes_left'/>
+          <p>{text}</p>
+        </div>
+        <h1>{name}</h1>
+        <h3>{designation}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default TestimonialCard
--- src/sections/Testimonials/Testimonials.js
+++ src/sections/Testimonials/Testimonials.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Slider from "react-slick";
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
+
+import TestimonialCard from './TestimonialCard';
 import './Testimonials.css'
-const Testimonials = () => {
-  return (
-    <div id='testimonials'>Testimonials</div>
-  )
+import { testimonialsData } from '../../data/testimonialsData';
+
+import test_bg from '../../assets/svg/test_bg.svg'
+
+function Testimonials() {
+
+    const sliderRef = useRef();
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        arrows: true,
+        fade: false,
+        pauseOnHover: false,
+        adaptiveHeight: true
+    };
+
+    const gotoNext = () => {
+        sliderRef.current.slickNext();
+    }
+
+    const gotoPrev = () => {
+        sliderRef.current.slickPrev();
+    }
+
+    return (
+        <div className='testimonials' id="testimonials">
+          <img src={test_bg} alt="" className='test_bg'/>  
+          <div className='testimonials__container'>
+            <div className="ps__header">Testimonials</div>
+              <div className='testimonials_section'>
+                  <IoIosArrowDropleftCircle onClick={gotoPrev} className='testimonial__prev'/>
+                  <div className='testimonials__slider'>
+                      <Slider {...settings} ref={sliderRef}>
+                          {testimonialsData && testimonialsData.map((test) => (
+                              <TestimonialCard 
+                                  id={test.id}
+                                  key={test.id}
+                                  name={test.name}
+                                  text={test.text}
+                                  designation={test.designation}
+                              />
+                          ))}
+                      </Slider>
+                  </div>
+                  <IoIosArrowDroprightCircle onClick={gotoNext} className='testimonial__next'/>
+              </div>
+          </div>
+        </div>
+    )
 }
 
 export default Testimonials
