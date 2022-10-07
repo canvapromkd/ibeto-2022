92ef134246d46a1efc15b266565b67ff13a34c7b
Hemanth R <hemanthrt18@gmail.com>
2022-10-07 15:07:42 +0530

    video

--- src/sections/Landing/Landing.js
+++ src/sections/Landing/Landing.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Aos from "aos";
+import { AiFillPauseCircle, AiFillPlayCircle } from "react-icons/ai";
+
 import "./Landing.css";
 import ibeto_22 from "../../assets/video/New.mp4";
-import Aos from "aos";
-import { useEffect } from "react";
-import { useState } from "react";
+
 const Landing = () => {
   const [muted, setMuted] = useState(true);
+  const [paused, setPaused] = useState(true);
+
+  const vidRef = useRef(null);
+
   useEffect(() => {
     Aos.init({ duration: 1100 });
     setMuted(false);
+
   }, []);
+
+  const handlePlay = () => {
+    vidRef.current.play();
+    setPaused(false)
+  }
+  const handlePause = () => {
+    vidRef.current.pause();
+    setPaused(true)
+  }
+
   return (
     <div className="landing_main" id="home">
+      {paused ? (
+        <AiFillPlayCircle onClick={handlePlay} className="video_icon"/>
+      ) : (
+        <AiFillPauseCircle onClick={handlePause} className="video_icon"/>
+      )}
       <div data-aos="zoom-in">
-        <video className="video_container" autoPlay loop playsInline muted={muted}>
+        <video ref={vidRef} className="video_container" loop playsInline muted={muted}>
           <source src={ibeto_22} type="video/Mp4" />
         </video>
       </div>
